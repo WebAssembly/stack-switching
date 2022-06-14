af6752fcd596ae2b0ba21f75c3842b728d215356
fgmccabe <fgmccabe@users.noreply.github.com>
2022-06-14 19:03:22 +0000

    deploy: 5ab6f5762719f63ac37a536f050b7c64a9a7fdd2

--- core/_static/documentation_options.js
+++ core/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.1 + function references + tail calls + stack-switching (Draft 2022-04-27)',
+    VERSION: '1.1 + function references + tail calls + stack-switching (Draft 2022-06-14)',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
