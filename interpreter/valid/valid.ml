697ac8956714963a541d0254e200343743598c36
Daniel Hillerström <daniel.hillerstrom@ed.ac.uk>
2023-10-18 15:38:19 +0200

    Fix nit

--- interpreter/valid/valid.ml
+++ interpreter/valid/valid.ml
@@ -196,7 +196,7 @@ let check_memory_type (c : context) (mt : memory_type) at =
 
 let check_tag_type (c : context) (et : tag_type) at =
   match et with
-  | TagT ft -> check_heap_type c ft at
+  | TagT ht -> check_heap_type c ht at
 
 let check_global_type (c : context) (gt : global_type) at =
   let GlobalT (_mut, t) = gt in
