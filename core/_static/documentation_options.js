138990ea5b966a810e495afd8bf05b0ea27ba98e
slindley <slindley@users.noreply.github.com>
2024-09-28 23:14:17 +0000

    deploy: a929dc4d36ff19d3a9ed9bfdf37ce0f11a9c8426

--- core/_static/documentation_options.js
+++ core/_static/documentation_options.js
@@ -1,12 +1,14 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.1 + function references + tail calls + stack-switching (Draft 2022-10-31)',
-    LANGUAGE: 'None',
+    VERSION: '3.0 + stack-switching (Draft 2024-09-28)',
+    LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
     FILE_SUFFIX: '.html',
     LINK_SUFFIX: '.html',
     HAS_SOURCE: false,
     SOURCELINK_SUFFIX: '.txt',
-    NAVIGATION_WITH_KEYS: false
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
 };
